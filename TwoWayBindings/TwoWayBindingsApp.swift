34e0abc24d37bedcdda269ac02415e5d952aef86
ronlemire2 <ronlemire2@gmail.com>
2025-09-01 12:37:42 -0700

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Ron Lemire on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
